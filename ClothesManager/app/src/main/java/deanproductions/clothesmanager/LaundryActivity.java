edbd485737a3a5498cf6d523645d3cbf4a7a17ef
AngelaRg <angela_9412@hotmail.com>
2017-07-20 18:35:06 +0200

    Created Activities

---
+++ ClothesManager/app/src/main/java/deanproductions/clothesmanager/LaundryActivity.java
@@ -0,0 +1,13 @@
+package deanproductions.clothesmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaundryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laundry);
+    }
+}
